a964b4c98fd677f42908a343915db98e37b74f85
freddy xu <acxpher@gmail.com>
2017-05-26 11:23:40 +1000

    Replace html with php

--- WED Assignment/index.php
+++ WED Assignment/index.php
@@ -1,4 +1,5 @@
-<?php include 'header.php' ?>
+<?php include'header.php';
+?>
 
 <script src="js/responsiveslides.min.js"></script>
 <script>
@@ -45,6 +46,101 @@
 	      </ul>
 	  </div>
   </div>
+
+
+<!-- timeline -->
+
+<section id="cd-timeline" class="cd-timeline-container">
+		<div class="cd-timeline-block">
+			<div class="cd-timeline-img ironman-color">
+
+			</div> <!-- cd-timeline-img -->
+
+			<div class="cd-timeline-content">
+				<img src="images/iron-poster.jpg">
+				<h2>Iron Man </h2>
+				<p>2008's Iron Man tells the story of Tony Stark, a billionaire industrialist and genius inventor who is kidnapped and forced to build a devastating weapon. Instead, using his intelligence and ingenuity, Tony builds a high-tech suit of armor and escapes captivity. When he uncovers a nefarious plot with global implications, he dons his powerful armor and vows to protect the world as Iron Man.</p>
+				<a href="http://marvel.com/movies/movie/19/iron_man" class="cd-read-more">Read more</a>
+				<span class="cd-date">May 2, 2008</span>
+			</div> <!-- cd-timeline-content -->
+		</div> <!-- cd-timeline-block -->
+
+		<div class="cd-timeline-block">
+			<div class="cd-timeline-img hulk-color">
+
+			</div> <!-- cd-timeline-img -->
+
+			<div class="cd-timeline-content">
+				<img src="images/hulk-poster.jpg">
+				<h2>The Incredible Hulk</h2>
+				<p>"The Incredible Hulk" kicks off an all-new, explosive and action-packed epic of one of the most popular super heroes of all time. In this new beginning, scientist Bruce Banner (Edward Norton) desperately hunts for a cure to the gamma radiation that poisoned his cells and unleashes the unbridled force of rage within him: The Hulk.</p>
+				<a href="http://marvel.com/movies/movie/23/the_incredible_hulk" class="cd-read-more">Read more</a>
+				<span class="cd-date">Jun 13, 2008</span>
+			</div> <!-- cd-timeline-content -->
+		</div> <!-- cd-timeline-block -->
+
+		<div class="cd-timeline-block">
+			<div class="cd-timeline-img ironman-color">
+
+			</div> <!-- cd-timeline-img -->
+
+			<div class="cd-timeline-content">
+				<img src="images/iron2-poster.jpg">
+				<h2>Iron Man 2</h2>
+				<p>Billionaire industrialist Tony Stark (played by Robert Downey, Jr.) returns to the big screen--and the Iron Man armor. Joined by Pepper Potts (Gwyneth Paltrow) and James Rhodes (Don Cheadle), expect big-time action and adventure in the Mighty Marvel Manner now available on Blu-ray, DVD and Digital Download.</p>
+				<a href="https://marvel.com/movies/movie/130/iron_man_2" class="cd-read-more">Read more</a>
+				<span class="cd-date">May 7, 2010</span>
+			</div> <!-- cd-timeline-content -->
+		</div> <!-- cd-timeline-block -->
+
+		<div class="cd-timeline-block">
+			<div class="cd-timeline-img thor-color">
+
+			</div> <!-- cd-timeline-img -->
+
+			<div class="cd-timeline-content">
+				<img src="images/thor-poster.jpg">
+				<h2>Thor</h2>
+				<p>The epic adventure "Thor" spans the Marvel Universe from present day Earth to the realm of Asgard. At the center of the story is The Mighty Thor, a powerful but arrogant warrior whose reckless actions reignite an ancient war. Thor is cast down to Earth and forced to live among humans as punishment. </p>
+				<a href="https://marvel.com/movies/movie/36/thor" class="cd-read-more">Read more</a>
+				<span class="cd-date">May 6, 2011</span>
+			</div> <!-- cd-timeline-content -->
+		</div> <!-- cd-timeline-block -->
+
+		<div class="cd-timeline-block">
+			<div class="cd-timeline-img cap-color">
+
+			</div> <!-- cd-timeline-img -->
+
+			<div class="cd-timeline-content">
+				<img src="images/cap-poster.jpg">
+				<h2>Captain America: The First Avenger</h2>
+				<p>Marvel's "Captain America: The First Avenger" focuses on the early days of the Marvel Universe when Steve Rogers volunteers to participate in an experimental program that turns him into the Super Soldier known as Captain America.</p>
+				<a href="https://marvel.com/movies/movie/125/captain_america_the_first_avenger" class="cd-read-more">Read more</a>
+				<span class="cd-date">Jul 22, 2011</span>
+
+			</div> <!-- cd-timeline-content -->
+		</div> <!-- cd-timeline-block -->
+
+		<div class="cd-timeline-block">
+			<div class="cd-timeline-img avg-color">
+
+			</div> <!-- cd-timeline-img -->
+
+			<div class="cd-timeline-content">
+				<img src="images/avg-poster.jpg">
+				<h2>Marvel's The Avengers</h2>
+				<p>Marvel Studios presents in association with Paramount Pictures "Marvel's The Avengers"--the super hero team up of a lifetime, featuring iconic Marvel super heroes Iron Man, the Incredible Hulk, Thor, Captain America, Hawkeye and Black Widow.</p>
+				<a href="https://marvel.com/movies/movie/152/marvels_the_avengers" class="cd-read-more">Read more</a>
+				<span class="cd-date">May 4, 2012</span>
+			</div> <!-- cd-timeline-content -->
+		</div> <!-- cd-timeline-block -->
+	</section> <!-- cd-timeline -->
+
+
+
+
+
 <!---->
 <script src="js/bootstrap.js"> </script>
 
@@ -52,6 +148,4 @@
 
 
 <!---->
-
-<!---->
-<?php include'footer.php' ?>
+<?php include'footer.php';?>
