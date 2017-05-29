dbf03781b5829fe9f8257294376326449bf1c2fe
XUYUAN GAO <gaoxuyuan55@gmail.com>
2017-05-29 15:49:04 +1000

    General Fix

---
+++ WED 222222/header.php
@@ -0,0 +1,78 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Contact</title>
+
+<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+<link rel="stylesheet" type="text/css" href="css/flexslider.css">
+<link rel="stylesheet" type="text/css" href="css/jquery-ui.css">
+<link rel="stylesheet" type="text/css" href="css/memenu.css">
+<link rel="stylesheet" type="text/css" href="css/style.css">
+<link rel="stylesheet" type="text/css" href="css/timeline.css">
+<link rel="stylesheet" type="text/css" href="css/">
+<link rel="stylesheet" type="text/css" href="css/">
+<link rel="stylesheet" type="text/css" href="css/">
+<link rel="stylesheet" type="text/css" href="css/">
+<link rel="stylesheet" type="text/css" href="css/">
+
+
+<script type="text/javascript" src="js/bootstrap.js"></script>
+<script type="text/javascript" src="js/custom.js"></script>
+<script type="text/javascript" src="js/imagezoom.js"></script>
+<script type="text/javascript" src="js/jquery-ui.min.js"></script>
+<script type="text/javascript" src="js/jquery.flexisel.js"></script>
+<script type="text/javascript" src="js/jquery.flexslider.js"></script>
+<script type="text/javascript" src="js/jquery.jscrollpane.min.js"></script>
+<script type="text/javascript" src="js/jquery.min.js"></script>
+<script type="text/javascript" src="js/responsiveslides.min.js"></script>
+<script type="text/javascript" src="js/simpleCart.min.js"></script>
+<script type="text/javascript" src="js/timeline.js"></script>
+<script type="text/javascript" src="js/modernizr.js"></script>
+
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+
+
+
+</head>
+<body>
+<!--header-->
+<div class="header-top">
+	 <div class="header-bottom">
+				<div class="logo">
+					<h1><a href="index.php">MARVEL</a></h1>
+
+				</div>
+			 <!---->
+			 <div class="top-nav">
+				<ul class="memenu skyblue"><li><a href="welcome.php">Home</a></li>
+					<li class="grid"><a href="news.php">News</a></li>
+					<li class="grid"><a href="product.php">Products</a></li>
+					<li class="grid"><a href="contact.php">Contact</a></li>
+					<li class="grid"><a href="checkout.php">checkout</a></li>
+
+				</ul>
+				<div class="clearfix"> </div>
+			 </div>
+			 <!---
+			 <div class="cart box_1">
+				 <a href="checkout.php">
+					<div class="total">
+					<span class="simpleCart_total"></span> (<span id="simpleCart_quantity" class="simpleCart_quantity"></span>)</div>
+					<span class="glyphicon glyphicon-shopping-cart" aria-hidden="true"></span>
+				</a>
+				<p><a href="javascript:;" class="simpleCart_empty">Empty Cart</a></p>
+			 	<div class="clearfix"> </div>
+			 </div>
+   --->
+			 <div class="clearfix"> </div>
+			 <!---->
+			 </div>
+			<div class="clearfix"> </div>
+</div>
+
+<!---->
+</body>
+</html>
