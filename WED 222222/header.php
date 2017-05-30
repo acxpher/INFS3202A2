737a7706d7367dbbc6e3f6f318c2181b1ab1749a
XUYUAN GAO <gaoxuyuan55@gmail.com>
2017-05-30 12:35:28 +1000

    fix header

--- WED 222222/header.php
+++ WED 222222/header.php
@@ -47,7 +47,8 @@
 				</div>
 			 <!---->
 			 <div class="top-nav">
-				<ul class="memenu skyblue"><li><a href="welcome.php">Home</a></li>
+				<ul class="memenu skyblue">
+					<li class="grid"><a href="welcome.php">Home</a></li>
 					<li class="grid"><a href="news.php">News</a></li>
 					<li class="grid"><a href="product.php">Products</a></li>
 					<li class="grid"><a href="contact.php">Contact</a></li>
@@ -55,20 +56,7 @@
 
 				</ul>
 				<div class="clearfix"> </div>
-			 </div>
-			 <!---
-			 <div class="cart box_1">
-				 <a href="checkout.php">
-					<div class="total">
-					<span class="simpleCart_total"></span> (<span id="simpleCart_quantity" class="simpleCart_quantity"></span>)</div>
-					<span class="glyphicon glyphicon-shopping-cart" aria-hidden="true"></span>
-				</a>
-				<p><a href="javascript:;" class="simpleCart_empty">Empty Cart</a></p>
-			 	<div class="clearfix"> </div>
-			 </div>
-   --->
-			 <div class="clearfix"> </div>
-			 <!---->
+
 			 </div>
 			<div class="clearfix"> </div>
 </div>
