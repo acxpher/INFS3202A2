fe32fa2b1e86d547b1811bd27ec45856fe157155
XUYUAN GAO <gaoxuyuan55@gmail.com>
2017-05-30 10:25:01 +1000

    News fix

--- WED 222222/news.php
+++ WED 222222/news.php
@@ -24,7 +24,7 @@
 					<p id="spider"></p><br>
 
 					<button class="btn btn-lg btn-default" type="button" onclick="loadDoc()">Read more</button>
-					<button class="btn btn-lg btn-default" type="button" id="less">Close</button><br/>
+					<button class="btn btn-lg btn-default" type="button" onclick="loadDoc4()">Close</button><br/>
 
 
 
@@ -39,11 +39,20 @@
 						  xhttp.open("GET", "ajax/spider_x.txt", true);
 						  xhttp.send();
 					}
-					$(document).ready(function (){
-					    $("#less").click(function(){
-					        $("#spider").toggle();
-					    });
-						});
+
+					function loadDoc4() {
+						  var xhttp = new XMLHttpRequest();
+						  xhttp.onreadystatechange = function() {
+						    if (this.readyState == 4 && this.status == 200) {
+						     document.getElementById("spider").innerHTML = this.responseText;
+						    }
+						  };
+						  xhttp.open("GET", "ajax/empty.txt", true);
+						  xhttp.send();
+					}
+
+
+
 						</script>
 
 						<script>
@@ -78,16 +87,29 @@
 						  xhttp.open("GET", "ajax/guard.txt", true);
 						  xhttp.send();
 					}
+
+						function loadDoc5() {
+						  var xhttp = new XMLHttpRequest();
+						  xhttp.onreadystatechange = function() {
+						    if (this.readyState == 4 && this.status == 200) {
+						     document.getElementById("guard").innerHTML = this.responseText;
+						    }
+						  };
+						  xhttp.open("GET", "ajax/empty.txt", true);
+						  xhttp.send();
+					}
+
+
 					$(document).ready(function(){
-					    $("#less").click(function loadDoc2(){
+					    $("#less").click(function(){
 					        $("#guard").toggle();
 					    });
 						});
 						</script>
 
 					<button class="btn btn-lg btn-default" type="button" onclick="loadDoc2()">Read more</button>
-					<button class="btn btn-lg btn-default" type="button" id="less">Close</button><br/>
-					
+					<button class="btn btn-lg btn-default" type="button" onclick="loadDoc5()">Close</button><br/>
+
 
 					<br><br><br><br>
 
@@ -114,8 +136,18 @@
 						  xhttp.open("GET", "ajax/rdj.txt", true);
 						  xhttp.send();
 					}
+					function loadDoc6() {
+						  var xhttp = new XMLHttpRequest();
+						  xhttp.onreadystatechange = function() {
+						    if (this.readyState == 4 && this.status == 200) {
+						     document.getElementById("rdj").innerHTML = this.responseText;
+						    }
+						  };
+						  xhttp.open("GET", "ajax/empty.txt", true);
+						  xhttp.send();
+					}
 					$(document).ready(function(){
-					    $("#less").click(function loadDoc3(){
+					    $("#less").click(function(){
 					        $("#rdj").toggle();
 					    });
 						});
@@ -123,7 +155,7 @@
 
 
 					<button class="btn btn-lg btn-default" type="button" onclick="loadDoc3()">Read more</button>
-					<button class="btn btn-lg btn-default" type="button" id="less">Close</button><br/>
+					<button class="btn btn-lg btn-default" type="button" onclick="loadDoc6()">Close</button><br/>
 
 
 
